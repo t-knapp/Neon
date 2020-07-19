528739d38df4ca5be42650dbd6416af592e5f5ae
t-knapp <tobias-knapp@web.de>
2020-07-19 22:24:10 +0200

    Use plain bootstrap w/o wrapping lib

--- Neon.Client/src/components/Management/Management.tsx
+++ Neon.Client/src/components/Management/Management.tsx
@@ -1,18 +1,12 @@
-import React, { ReactElement } from 'react';
-import { boundMethod } from 'autobind-decorator'
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import React, { ReactElement, ChangeEvent, FormEvent } from 'react';
+import { boundMethod } from 'autobind-decorator';
 
 type Props = { };
 type State = {
     name: string,
     displayTime: number,
     context: string,
+    file: File,
     notBefore: string,
     notAfter: string
 };
@@ -22,107 +16,116 @@ export default class Management extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            name: 'Name',
+            name: '',
             displayTime: 10,
             context: 'Default',
+            file: null,
             notBefore: null,
             notAfter: null
-        }
+        };
     }
 
     public render(): ReactElement {
         return (
             <div className='ManagementComponent'>
-                <Container className='p-3'>
-                    <Jumbotron>
-                        <h1 className='header'>Verwaltung</h1>
-                        <Form onSubmit={this._onFormSubmit}>
-                            <Form.Group as={Row} controlId='formHorizontalName'>
-                                <Form.Label column sm={4}>Name</Form.Label>
-                                <Col sm={8}>
-                                    <Form.Control type='text' placeholder='Name' onChange={this._onNameChanged} />
-                                </Col>
-                            </Form.Group>
-
-                            <Form.Group as={Row} controlId='formHorizontalDisplayTime'>
-                                <Form.Label column sm={4}>Anzeigedauer</Form.Label>
-                                <Col sm={8}>
-                                    <Form.Control type='number' placeholder='Anzeigedauer in Sekunden' />
-                                </Col>
-                            </Form.Group>
-                            
-                            <Form.Group as={Row} controlId='formHorizontalContext'>
-                                <Form.Label column sm={4}>Kontext</Form.Label>
-                                <Col sm={8}>
-                                    <Form.Control type='text' placeholder='Default' readOnly />
-                                </Col>
-                            </Form.Group>
-
-                            <Form.Group as={Row} controlId='formHorizontalFile'>
-                                <Form.Label column sm={4}>Bilddatei</Form.Label>
-                                <Col sm={8}>
-                                    <Form.File id='exampleFormControlFile' />
-                                </Col>
-                            </Form.Group>
-
-                            <Form.Group as={Row} controlId='formHorizontalNotBefore'>
-                                <Form.Label column sm={4}>Nicht zeigen vor Datum (optional)</Form.Label>
-                                <Col sm={8}>
-                                    <Form.Control type='text' placeholder='' />
-                                </Col>
-                            </Form.Group>
-
-                            <Form.Group as={Row} controlId='formHorizontalNotAfter'>
-                                <Form.Label column sm={4}>Nicht zeigen nach Datum (optional)</Form.Label>
-                                <Col sm={8}>
-                                    <Form.Control type='text' placeholder='' />
-                                </Col>
-                            </Form.Group>
-
-                            <Form.Group as={Row}>
-                                <Col sm={{ span: 10, offset: 2 }}>
-                                    <Button type='submit'>Hochladen</Button>
-                                </Col>
-                            </Form.Group>
-                        </Form>
-                    </Jumbotron>
-                </Container>
+                <div className='container'>
+                <form onSubmit={this._onFormSubmit}>
+                    <div className='form-group row'>
+                        <label className='col-sm-4 col-form-label'>Name</label>
+                        <div className='col-sm-8'>
+                            <input type='text' className='form-control' value={this.state.name} onChange={this._onNameChanged} />
+                        </div>
+                    </div>
+                    <div className='form-group row'>
+                        <label className='col-sm-4 col-form-label'>Kontext</label>
+                        <div className='col-sm-8'>
+                            <input type='text' className='form-control' value={this.state.context} onChange={this._onContextChanged} readOnly={true} />
+                        </div>
+                    </div>
+                    <div className='form-group row'>
+                        <label className='col-sm-4 col-form-label'>Anzeigezeit (Sekunden)</label>
+                        <div className='col-sm-8'>
+                            <input type='number' min='5' max='300' step='1' className='form-control' value={this.state.displayTime} onChange={this._onDisplayTimeChanged} />
+                        </div>
+                    </div>
+                    <div className='form-group row'>
+                        <label className='col-sm-4 col-form-label'>Bilddatei</label>
+                        <div className='col-sm-8'>
+                            <input type='file' className='form-control' onChange={this._onFileChanged} />
+                        </div>
+                    </div>
+                    <div className='form-group row'>
+                        <label className='col-sm-4 col-form-label'>Nicht anzeigen vor (optional)</label>
+                        <div className='col-sm-8'>
+                            <input type='date' className='form-control' value={this.state.notBefore} onChange={this._onNotBeforeChanged} />
+                        </div>
+                    </div>
+                    <div className='form-group row'>
+                        <label className='col-sm-4 col-form-label'>Nicht anzeigen nach (optional)</label>
+                        <div className='col-sm-8'>
+                            <input type='date' className='form-control' value={this.state.notAfter} onChange={this._onNotAfterChanged} />
+                        </div>
+                    </div>
+                    <div className='form-group row'>
+                        <div className='col-sm-8'>
+                            <button type='submit' className='btn btn-primary'>Speichern</button>
+                        </div>
+                    </div>
+                </form>
+                </div>
             </div>
         );
     }
 
     @boundMethod
-    private _onNameChanged(): void {
-
+    private _onNameChanged(event: ChangeEvent<HTMLInputElement>): void {
+        this.setState({name: event.target.value});
     }
 
     @boundMethod
-    private _onDisplayTimeChanged(): void {
-
+    private _onDisplayTimeChanged(event: ChangeEvent<HTMLInputElement>): void {
+        this.setState({displayTime: parseInt(event.target.value, 10)});
     }
 
     @boundMethod
-    private _onContextChanged(): void {
-
+    private _onContextChanged(event: ChangeEvent<HTMLInputElement>): void {
+        this.setState({context: event.target.value});
     }
 
     @boundMethod
-    private _onFileChanged(): void {
-
+    private _onFileChanged(event: ChangeEvent<HTMLInputElement>): void {
+        this.setState({file: event.target.files[0]});
     }
 
     @boundMethod
-    private _onNotBeforeChanged(): void {
-
+    private _onNotBeforeChanged(event: ChangeEvent<HTMLInputElement>): void {
+        this.setState({notBefore: event.target.value});
     }
 
     @boundMethod
-    private _onNotAfterChanged(): void {
-
+    private _onNotAfterChanged(event: ChangeEvent<HTMLInputElement>): void {
+        this.setState({notAfter: event.target.value});
     }
 
     @boundMethod
-    private _onFormSubmit(event: React.FormEvent<HTMLElement>): void {
+    private _onFormSubmit(event: FormEvent<HTMLElement>): void {
         event.preventDefault();
+
+        // TODO: Move to service
+        const formData: FormData = new FormData();
+        formData.append('Name', this.state.name);
+        formData.append('ContextName', this.state.context);
+        formData.append('DisplayTime', this.state.displayTime.toString());
+        formData.append('Image', this.state.file);
+        if (this.state.notBefore)
+            formData.append('NotBefore', this.state.notBefore);
+
+        if (this.state.notAfter)
+            formData.append('NotAfter', this.state.notAfter);
+
+        fetch('https://localhost:5001/imageassets', {
+            method: 'POST',
+            body: formData
+        });
     }
 }
