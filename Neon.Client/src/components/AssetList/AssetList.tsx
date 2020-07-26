66f308468d97e82f2311a46c9771f2345f126f1e
t-knapp <tobias-knapp@web.de>
2020-07-26 21:48:14 +0200

    Change order

--- Neon.Client/src/components/AssetList/AssetList.tsx
+++ Neon.Client/src/components/AssetList/AssetList.tsx
@@ -70,30 +70,32 @@ export default class AssetList extends React.Component<Props, State> {
     }
 
     public render(): ReactElement {
-        const rows: ReactElement[] = this.state.imageAssets.map((asset: ImageAsset) => (
-            <tr key={asset.id}>
-                <td>
-                    <button type='button' onClick={() => this._onSetOrder(asset.id, asset.order - 1)} className='btn btn-outline-primary btn-sm'><i className='fas fa-arrow-up'></i></button>
-                    &nbsp;
-                    <button type='button' onClick={() => this._onSetOrder(asset.id, asset.order + 1)} className='btn btn-outline-primary btn-sm'><i className='fas fa-arrow-down'></i></button>
-                    &nbsp;
-                    {asset.name}
-                    &nbsp;
-                    {asset.order}
-                </td>
-                <td>{asset.displayTime}</td>
-                <td>{asset.notBefore ? moment.utc(asset.notBefore).format('DD.MM.YYYY') : ''}</td>
-                <td>{asset.notAfter ? moment.utc(asset.notAfter).format('DD.MM.YYYY') : ''}</td>
-                <td>{asset.isActive ? <span className='badge badge-primary'>Aktiv</span> : <span className='badge badge-secondary'>Inaktiv</span>}</td>
-                <td>
-                    <button type='button' onClick={() => this._onSetActive(asset.id, !asset.isActive)} className='btn btn-outline-primary btn-sm'>
-                        {asset.isActive ? 'Aus' : 'Ein'}
-                    </button>
-                    &nbsp;
-                    <button type='button' onClick={() => this._onDelete(asset.id)} className='btn btn-outline-danger btn-sm'><i className='fas fa-trash-alt'></i></button>
-                </td>
-            </tr>
-        ));
+        const rows: ReactElement[] = this.state.imageAssets.map((asset: ImageAsset, index: number) => {
+            const beforeOrder: number = ((this.state.imageAssets[index - 1]?.order) ?? asset.order) - 1;
+            const afterOrder: number = ((this.state.imageAssets[index + 1]?.order) ?? asset.order) + 1;
+            return (
+                <tr key={asset.id}>
+                    <td>
+                        <button type='button' onClick={() => this._onSetOrder(asset.id, beforeOrder)} className='btn btn-outline-primary btn-sm' disabled={index === 0}><i className='fas fa-arrow-up'/></button>
+                        &nbsp;
+                        <button type='button' onClick={() => this._onSetOrder(asset.id, afterOrder)} className='btn btn-outline-primary btn-sm' disabled={index === this.state.imageAssets.length - 1}><i className='fas fa-arrow-down'/></button>
+                        &nbsp;
+                        {asset.name}
+                    </td>
+                    <td>{asset.displayTime}</td>
+                    <td>{asset.notBefore ? moment.utc(asset.notBefore).format('DD.MM.YYYY') : ''}</td>
+                    <td>{asset.notAfter ? moment.utc(asset.notAfter).format('DD.MM.YYYY') : ''}</td>
+                    <td>{asset.isActive ? <span className='badge badge-primary'>Aktiv</span> : <span className='badge badge-secondary'>Inaktiv</span>}</td>
+                    <td>
+                        <button type='button' onClick={() => this._onSetActive(asset.id, !asset.isActive)} className='btn btn-outline-primary btn-sm'>
+                            {asset.isActive ? 'Aus' : 'Ein'}
+                        </button>
+                        &nbsp;
+                        <button type='button' onClick={() => this._onDelete(asset.id)} className='btn btn-outline-danger btn-sm'><i className='fas fa-trash-alt'/></button>
+                    </td>
+                </tr>
+            );
+        });
         return (
             <table className='table table-hover'>
                 <thead>
