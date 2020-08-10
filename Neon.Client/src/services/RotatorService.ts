6cacf3c173a7d20dc7ce622129addc0d88ec1a9e
t-knapp <tobias-knapp@web.de>
2020-08-10 22:20:47 +0200

    Consider dates when displing assets

--- Neon.Client/src/services/RotatorService.ts
+++ Neon.Client/src/services/RotatorService.ts
@@ -1,4 +1,5 @@
 import { observable, action } from 'mobx';
+import moment from 'moment';
 import HttpImageAssetProvider from '../providers/HttpImageAssetProvider';
 import ImageAsset from '../models/ImageAsset';
 
@@ -24,7 +25,7 @@ export default class RotatorService {
     @action
     private async _rotate(): Promise<void> {
         try {
-            const assetList: ImageAsset[] = (await this._provider.allAsync()).filter((a) => a.isActive);
+            const assetList: ImageAsset[] = (await this._provider.allAsync()).filter(this.filter);
             this._index = (this._index + 1) >= assetList.length ? 0 : this._index + 1;
             const asset: ImageAsset = assetList[this._index];
             this.currentImage = await this._provider.oneContentAsync(asset.id);
@@ -33,5 +34,11 @@ export default class RotatorService {
         } catch (ex) {
             console.warn('rotate', ex);
         }
+    }
+
+    private filter(asset: ImageAsset): boolean {
+        return asset.isActive
+            && asset.notBefore ? (moment.utc(asset.notBefore).isBefore(moment())) : true
+            && asset.notAfter ? (moment.utc(asset.notAfter).isAfter(moment())) : true;
     }
 }
