d068c93aabf9e95595cb3095836f9f18257ae978
t-knapp <tobias-knapp@web.de>
2020-09-19 18:36:08 +0200

    Fixed multiple starts of rotator

--- Neon.Client/src/services/RotatorService.ts
+++ Neon.Client/src/services/RotatorService.ts
@@ -1,19 +1,17 @@
 import { observable, action } from 'mobx';
 import moment from 'moment';
-import { Mutex } from 'async-mutex';
 import HttpImageAssetProvider from '../providers/HttpImageAssetProvider';
 import ImageAsset from '../models/ImageAsset';
 
 export default class RotatorService {
     @observable public currentImage: string;
 
-    private readonly _mutex: Mutex;
     private _index: number;
     private _running: boolean;
     private _provider: HttpImageAssetProvider;
+    private _cancellation: () => void;
 
     constructor(provider: HttpImageAssetProvider) {
-        this._mutex = new Mutex();
         this._index = 0;
         this._running = false;
         this._provider = provider;
@@ -21,29 +19,16 @@ export default class RotatorService {
 
     public start(): void {
         this._running = true;
-        this._rotate(); // Fire & Forget
+        this._rotate(new Promise((resolve) => { this._cancellation = resolve; }));
     }
 
     public stop(): void {
         this._running = false;
-    }
-
-    private async _blobToBase64(blob: Blob): Promise<string> {
-        return new Promise((resolve, reject) => {
-            const reader: FileReader = new FileReader();
-            reader.onloadend = () => {
-                resolve(reader.result as string);
-            };
-            reader.onerror = () => {
-                reject(reader.error);
-            };
-            // result will be string @see: https://developer.mozilla.org/en-US/docs/Web/API/FileReader/result
-            reader.readAsDataURL(blob);
-        });
+        this._cancellation();
     }
 
     @action
-    private async _rotate(): Promise<void> {
+    private async _rotate(cancellationPromise: Promise<void>): Promise<void> {
         while (this._running) {
             try {
                 const assetList: ImageAsset[] = (await this._provider.allAsync())
@@ -51,17 +36,17 @@ export default class RotatorService {
                     .sort(this._sort);
                 if (assetList.length === 0) {
                     this.currentImage = null;
-                    await new Promise((resolve) => window.setTimeout(resolve, 5000));
+                    await Promise.race([new Promise((resolve) => window.setTimeout(resolve, 5000)), cancellationPromise]);
                     continue;
                 }
                 this._index = (this._index + 1) >= assetList.length ? 0 : this._index + 1;
                 const asset: ImageAsset = assetList[this._index];
                 this.currentImage = await this._blobToBase64(await this._provider.oneContentAsync(asset.id));
-                await new Promise((resolve) => window.setTimeout(resolve, asset.displayTime * 1000));
+                await Promise.race([new Promise((resolve) => window.setTimeout(resolve, asset.displayTime * 1000)), cancellationPromise]);
             } catch (ex) {
                 console.warn('Error while rotating:', ex);
                 this.currentImage = null;
-                await new Promise((resolve) => window.setTimeout(resolve, 5000));
+                await Promise.race([new Promise((resolve) => window.setTimeout(resolve, 5000)), cancellationPromise]);
             }
         }
     }
@@ -79,5 +64,19 @@ export default class RotatorService {
         if (lhs.order > rhs.order)
             return 1;
         return 0;
+    }
+
+    private async _blobToBase64(blob: Blob): Promise<string> {
+        return new Promise((resolve, reject) => {
+            const reader: FileReader = new FileReader();
+            reader.onloadend = () => {
+                resolve(reader.result as string);
+            };
+            reader.onerror = () => {
+                reject(reader.error);
+            };
+            // result will be string @see: https://developer.mozilla.org/en-US/docs/Web/API/FileReader/result
+            reader.readAsDataURL(blob);
+        });
     }
 }
